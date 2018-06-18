23f693dc29a83a8ca389ccfcec965b1a147dd268
kevin schmitt <kevin.schmitt.upjv@gmail.com>
2018-06-18 20:39:55 +0200

    clean code useless

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-
+// import
 const express = require('express');
 const path = require('path');
 const app = express();
 const loto = require("./module/loto.js");
-
+// define dir view and css
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
+//routing
 app.get('/', (req, res) => res.render('index.ejs', {Numbers: loto.getNumbers()}));
 
 app.listen(3000, () =>
--- module/loto.js
+++ module/loto.js
@@ -1,30 +1,8 @@
-const readline = require('readline');
 const rand = require('random');
-//const server = require('server.js');
 
 module.exports = {
-	//  vérifie si on a gagné
-	winner: function(game, numbersGame)
-	{
-		console.log(game);
-		console.log(numbersGame);
-		let nb=0;
-		for(i=0;i<game.length;i++)
-		{
-			for(j=0;j<numbersGame.length;j++)
-		   {
-				if(game[i] == numbersGame[j]) nb++;
-		   }
-		}
 
-		if(game.length <= i)
-		{
-			(nb > 2) ? console.log("Vous avez " + nb + " bons numéros") : console.log("Game over!");
-		//	server.displayView();
-		}
-
-	},
-
+	//random numbers
 	getNumbers: function()
 	{
 		let limit = 7;
@@ -44,32 +22,4 @@ module.exports = {
 
 	},
 
-	// retourne un tableau avec des nombres aléatoires
-	numbersGame:function(numberList)
-	{
-
-		let numbersForWin = new Array();
-		console.log("Tirage des numéros -> ");
-		// on génère nos nombres
-		for(var end = 0;end < 7;end++)
-		{
-			nbRand = parseInt(Math.random()*48+1);
-			if (nbRand < 50)
-			{
-				console.log(nbRand);
-				numbersForWin.push(nbRand);
-			}
-		}
-
-		if(end == 7){
-			this.winner(numberList, numbersForWin);
-		}
-	},
-
-	// fonction principale
-	game: function(numberList)
-	{
-		//this.numbersGame(numberList);
-		this.getNumbers();
-	}
 }
